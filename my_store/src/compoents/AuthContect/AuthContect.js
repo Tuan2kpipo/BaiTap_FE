7be2fd33f63abff6c47dc0adf6e4d8c8831371a6
Tuan Nguyen <tuan2kpipo@gmail.com>
2022-12-05 08:46:12 +0700

    update name

--- my_store/src/compoents/AuthContect/AuthContect.js
+++
@@ -1,9 +0,0 @@
-export const saveAccesTokenTolS = (access_token) => {
-  localStorage.setItem("access_token", access_token);
-};
-
-export const clearAccesTokenForm = () => {
-  localStorage.removeItem("access_token");
-};
-
-export const getAccesToken = () => localStorage.getItem("access_token") || "";
